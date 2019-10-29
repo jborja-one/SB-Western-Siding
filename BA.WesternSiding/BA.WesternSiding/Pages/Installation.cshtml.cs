ebecd980f218385c6fdf66e34d207df8100df0be
Shawn Bailey <shawn@shawnwbailey.com>
2019-10-29 00:32:19 -0600

    SEO Windows

--- BA.WesternSiding/BA.WesternSiding/Pages/Installation.cshtml.cs
+++ BA.WesternSiding/BA.WesternSiding/Pages/Installation.cshtml.cs
@@ -2,16 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BA.WesternSiding.DataModels;
+using BA.WesternSiding.Adapters;
+using BA.WesternSiding.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using BA.Common.Services;
 
 namespace BA.WesternSiding.Pages
 {
     public class InstallationModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IConfiguration _config;
+        private readonly ISmtpService _smtpService;
 
+        [BindProperty]
+        public ContactUsModel contactUsModel { get; set; }
+
+        public InstallationModel(IConfiguration config, ISmtpService smtpService)
+        {
+            _config = config;
+            _smtpService = smtpService;
         }
+
+        public void OnGet() { }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                ContactUsAdapter _contactUs = new ContactUsAdapter(_config, _smtpService);
+                await _contactUs.CreateAndSendEmail(contactUsModel);
+                ViewData["Message"] = "Your message has been recieved.";
+                return Page();
+            }
+            else
+            {
+                ViewData["Message"] = "There was a problem.  Try again!";
+                return Page();
+            }
+        }
+
     }
 }
